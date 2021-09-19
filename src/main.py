01bc5b3477be596ea39970c6053dba703c9a5772
Eric Norman <eric@norman.zone>
2021-09-19 14:46:29 -0700

    Debug, verbose pass and test

--- src/main.py
+++ src/main.py
@@ -9,20 +9,49 @@ import click
 @click.group()
 @click.option("--debug", "-d", is_flag=True, help="Use debug mode")
 @click.option("--verbose", "-v", is_flag=True, help="Use verbose mode")
-def cli(debug, verbose):
-    click.echo("Debug mode is on") if debug else None
-    click.echo("Verbose mode is on") if verbose else None
+@click.pass_context
+def cli(ctx, debug, verbose):
+    """Pass general probram settings such as 'debug'
+
+    Example, to call 'sum' with 'debug' use:
+        'python ./src/main.py --debug sum'
+    """
+    ctx.ensure_object(dict)
+
+    ctx.obj["DEBUG"] = debug
+    ctx.obj["VERBOSE"] = verbose
+
+    if verbose:
+        click.echo(f"Verbose is on")
+        click.echo(f"Debug is {'on' if ctx.obj['DEBUG'] else 'off'}")
+    elif debug:
+        click.echo(f"Debug is 'on'")
+    else:
+        pass
 
 
 @cli.command()
-# @click.option("--items", nargs=2, type=click.Tuple([int, int]))
 @click.argument("items", nargs=-1, type=int)
-def sum(items):
-    # assert items
+@click.pass_context
+def sum(ctx, items):
+    """Sum any number of objects passed in via 'items'.
+
+    Params:
+        number items: numbers to sum
+            Note: use "--" before negatives
+                example: "sum -- 2 3 -1"
+        bool debug: debug mode via ctx
+        bool verbose: verbose mode via ctx"""
+    debug = ctx.obj["DEBUG"]
+    verbose = ctx.obj["VERBOSE"]
+
+    if verbose:
+        click.echo(f"items={items}")
+
     sum = reduce(lambda x, y: x + y, items)
     click.echo(f"sum={sum}")
     return sum
 
 
 if __name__ == "__main__":
-    cli()
+    cli(obj={})
--- tests/main/test_sum.py
+++ tests/main/test_sum.py
@@ -2,14 +2,16 @@
 
 
 from click.testing import CliRunner
+from src.main import cli
 
 
 def test_sum():
     runner = CliRunner()
-    result = runner.invoke(sum, ["2", "3"])
+    result = runner.invoke(cli, ["--debug", "sum", "2", "3"])
     assert result.exit_code == 0
     assert str(5) in result.output
+    assert "debug" in result.output.casefold()
 
-    result = runner.invoke(sum, ["--", "2", "3", "-1"])
+    result = runner.invoke(cli, ["sum", "--", "2", "3", "-1"])
     assert result.exit_code == 0
     assert str(4) in result.output
